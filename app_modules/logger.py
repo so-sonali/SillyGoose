78951b0f01e4431865d9df81efbe3f7f8ee5fd88
gsspc24 <gsspc24@buffalo.edu>
2025-04-11 19:11:35 -0400

    logger

--- app_modules/logger.py
+++ app_modules/logger.py
@@ -1,8 +1,17 @@
 from datetime import datetime
 import os
+from pathlib import Path
+from datetime import datetime
 
 def save_log(tool_name, result):
     os.makedirs("logs", exist_ok=True)
     with open("logs/calculation_history.txt", "a", encoding="utf-8") as file:
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         file.write(f"{now},{tool_name},{result}\n")
+        
+def save_log(name, content):
+    log_dir = Path("logs")
+    log_dir.mkdir(exist_ok=True)  # Create folder if missing
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    file_path = log_dir / f"{name}_{timestamp}.txt"
+    file_path.write_text(content.strip())
